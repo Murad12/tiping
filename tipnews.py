811f9f865b00b848e92617ec212d7a7db4034f99
Murad <murad@iMac-Murad.local>
2020-04-18 22:08:38 +0300

    first commit

---
+++ tipnews.py
@@ -0,0 +1,13 @@
+ import telebot
+
+bot = telebot.TeleBot("1097106929:AAHHULbtWtNkPVeqfzSHsl0ieGDSA6gSo2g")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
